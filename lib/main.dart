d18d599dd7ecce9e12c7e21b00c6a49a5aa8a447
Юлия Москалева <yulmosk@mail.ru>
2024-02-06 10:48:56 +0300

    Рома смотри

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sunny Stickers',
+      title: 'Sunny Stickers Animation',
       theme: AppTheme.darkTheme,
       home: const HomeScreen(),
     );
